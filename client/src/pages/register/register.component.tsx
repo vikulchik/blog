66ce3abd4dbfe44d69f016452b8ad621540c3cf4
viktoriia.ko <viktoriia.ko@voicespin.com>
2022-07-05 14:00:14 +0200

    added routers

---
+++ client/src/pages/register/register.component.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+export function Register(): JSX.Element {
+  return (
+    <div>
+      <h1>Register</h1>
+      <Link to="/">Homepage</Link>
+    </div>
+  );
+}
